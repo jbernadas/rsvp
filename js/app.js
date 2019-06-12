fe8556765ba16a31a94bb02236aabd7d5579380a
jbernadas <jb3rns@gmail.com>
2019-06-11 21:57:08 -0500

    Added hide those who have not responded

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckbox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckbox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckbox);
+mainDiv.insertBefore(div, ul);
+filterCheckbox.addEventListener('change', (e) => {
+  const isChecked = e.target.checked;
+  const lis = ul.children;
+  if (isChecked) {
+    for (let i = 0; i <= lis.length; i++) {
+      let li = lis[i];
+      if (li.className === 'responded') {
+        li.style.display = '';
+      } else {
+        li.style.display = 'none';
+      }
+    }
+  } else {
+    for (let i = 0; i <= lis.length; i++) {
+      let li = lis[i];
+      li.style.display = '';
+    }
+  }
+})
 
 const createLi = (text) => {
   const li = document.createElement('li');
@@ -66,6 +97,5 @@ ul.addEventListener('click', (e) => {
       li.removeChild(input);
       button.textContent = 'Edit';
     }
-    
   }  
 });
